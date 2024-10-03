205b5ab9945e2cb516263f618f9fc4d03d5747d9
tay3434 <tns5371@psu.edu>
2024-10-03 15:00:51 -0400

    changes for homework 6

--- src/counter-app.js
+++ src/counter-app.js
@@ -10,11 +10,17 @@ export class counterApp extends DDDSuper(LitElement) {
   constructor() {
     super();
     this.title = "";
+    this.counter = 0;
+    this.minNumber = 0;
+    this.maxNumber = 30;
   }
 
   static get properties() {
     return {
       title: { type: String },
+      counter: {type: Number, reflect: true },
+      minNumber: {type: Number},
+      maxNumber: {type: Number}
     };
   }
 
@@ -26,25 +32,88 @@ export class counterApp extends DDDSuper(LitElement) {
         color: var(--ddd-theme-primary);
         background-color: var(--ddd-theme-accent);
         font-family: var(--ddd-font-navigation);
-        font-size: var(--counter-app-font-size, var(--ddd-font-size-s));
+        font-size: var(--counter-app-font-size, var(--ddd-font-size-l));
       }
       .wrapper {
-        margin: var(--ddd-spacing-2);
-        padding: var(--ddd-spacing-4);
+        text-align: center;
+        margin: var(--ddd-spacing-4);
+        padding: var(--ddd-spacing-8);
       }
-      div {
-        padding: 0;
-        margin: 0;
+      .counter{
+        font-size: var(--ddd-font-size-l);
+      }
+      :host([counter="18"]) .counter {
+        color: var(--ddd-theme-default-athertonViolet);
+      }
+
+      :host([counter="21"]) .counter {
+        color: var(--ddd-theme-default-beaver70);
+      }
+      :host([counter="0"]) .counter {
+        color: var(--ddd-theme-default-futureLime);
+      }
+      :host([counter="30"]) .counter {
+        color: var(--ddd-theme-default-original87Pink);
+      } 
+     
+      button {
+        font-size: var(--ddd-font-size-s);
+        padding: var(--ddd-spacing-2);
+        background-color: var(--ddd-theme-default-slateMaxLight);
+        border: 1px solid var(--ddd-theme-default-slateLight);
+        border-radius: 8px;
+      }
+
+      button:hover {
+        background-color: var(--ddd-theme-default-creekLight);
+      }
+
+      button:focus {
+        outline: 2px solid var(--ddd-theme-default-creekTeal);
+      }
+      button:disabled {
+        background-color: var(--ddd-theme-default-disabled);
       }
     `];
   }
+  increment(){
+    if (this.counter < this.maxNumber) { 
+      this.counter++;
+    }
+  }
+    
+  decrement(){
+    if (this.counter > this.minNumber) { 
+      this.counter--;
+  }
+}
+updated(changedProperties) {
+  if (changedProperties.has('counter')) {
+    if (this.counter === this.maxNumber) {
+      this.makeItRain();
+    }
+  }
+}
+makeItRain() {
+  import("@haxtheweb/multiple-choice/lib/confetti-container.js").then(() => {
+    setTimeout(() => {
+      this.shadowRoot.querySelector("#confetti").setAttribute("popped", "");
+    }, 0);
+  });
+}
+  
 
   render() {
     return html`
 <div class="wrapper">
   <div>${this.title}</div>
+  <div class="counter">${this.counter}</div>
+  <button @click="${this.increment}" ?disabled="${this.counter === this.maxNumber}">+</button>
+  <button @click="${this.decrement}" ?disabled="${this.counter === this.minNumber}">-</button>
+  <confetti-container id="confetti"></confetti-container>
   <slot></slot>
-</div>`;
+</div>
+`;
   }
 
   /**
